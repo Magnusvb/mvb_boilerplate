ca3115fdc510da535ff8a9959bbc62c8d9a17271
Magnus von Bromsen <magnus@studioalice.se>
2014-10-31 15:48:48 +0100

    BrowserSync and Concat
    BrowserSync now reload browser after changes in html files. JS files is concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,38 +4,40 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    connect: {
-      server: {
-        options: {
-          port: 9001,
-          base: 'app'
-        }
-      }
-    },
-    browserSync: {
-      bsFiles: {
-        src : 'app/css/*.css'
-      },
-      options: {
-        watchTask: true,
-        server: {
-          baseDir: "app"
-        }
-      }
-    },
+    // connect: {
+    //   server: {
+    //     options: {
+    //       port: 9001,
+    //       base: 'app'
+    //     }
+    //   }
+    // },
+
     watch: {
       sass: {
         files: 'src/scss/**/*.scss',
         tasks: ['sass'],
       },
-      files: ['app/*.html'],
+      files: ['app/*.html', 'src/js/main.js'],
     },
+
+    concat: {
+      options: {
+        separator: '§§§',
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: ['src/js/*.js', 'src/**/*.js'],
+        dest: 'src/js/script.js'
+      }
+    },
+
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'script.js',
+        src: 'main.js',
         dest: 'app/js/script.min.js'
       }
     },
@@ -45,10 +47,22 @@ module.exports = function(grunt) {
         options: {
           banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
           require: 'susy',
-          style: 'compact'
+          style: 'compressed'
         },
         files: {
           'app/css/app.min.css' : 'src/scss/app.scss'
+        }
+      }
+    },
+
+    browserSync: {
+      bsFiles: {
+        src : ['app/css/*.css', 'app/*html']
+      },
+      options: {
+        watchTask: true,
+        server: {
+          baseDir: "app"
         }
       }
     }
@@ -57,19 +71,31 @@ module.exports = function(grunt) {
   // Start web server
   grunt.loadNpmTasks('grunt-contrib-connect');
 
-  // Watch changes
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  
+
+  // Load the sass plugin
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // Load the sass plugin
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  // Concat js files
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  // Watch changes
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Load Browsersync
   grunt.loadNpmTasks('grunt-browser-sync');
 
   // Default task(s).
-  grunt.registerTask('default', ['browserSync', 'watch', 'connect', 'uglify', 'sass' ]);
+  grunt.registerTask('default', [  
+    'browserSync',
+    'watch',
+    'sass', 
+    'connect', 
+    'uglify', 
+    'concat',
+    ]);
 
 };
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 mvb_boilerplate
 ===============
 
-Bare bone frontend boilerplate with grunt, susy and bourbon.
+Very simple and bare bone frontend boilerplate with grunt, susy and bourbon.
 
 **This is a work in progress and I just started the whole thing.
